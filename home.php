3e16ba280ac9ce007fdb0de3e7bff418509f0682
S4MU31M <samu.montiel11@gmail.com>
2022-10-08 14:09:54 -0300

    Update home.php
    se  mejora diseño de home

--- home.php
+++ home.php
@@ -1,17 +1,11 @@
 <?php require "partials/header.php"?>
 
-<div class="container pt-9 p-3" >
-  <div class="card text-center">
-    <div class="card-body">
-      <div class="card-body">
-        <div class="card-body">
-          <a href="add.php?"class="btn btn-secondary mb-2">Ingresar cliente</a>
-          <a href="index.php?"class="btn btn-secondary mb-2">Ver Clientes registrados</a>
-          <a href="delete.php?rut" class="btn btn-danger mb-2">Eliminar cliente</a>
-        </div>
-      </div>
-    </div>   
-  </div>  
+<div class="welcome d-flex align-items-center justify-content-center">
+  <div class="text-center">
+    <h1>Sistema de Registro de Clientes</h1>
+    <a class="btn btn-lg btn-dark" href="index.php">Ver Clientes Registrados</a>
+    <a class="btn btn-lg btn-dark" href="add.php">Agregar Cliente</a>
+  </div>
 </div>
 
 <?php require "partials/footer.php"?>
